396c1618f70ae67577e1d2a08d76ad1eb1c2facd
Bhavya Dadheech <70031238+Bhavya-Dadheech@users.noreply.github.com>
2024-05-11 14:09:00 +0530

    project update

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
 import { thunk } from "redux-thunk";
 import logger from "redux-logger";
-import { composeWithDevTools } from "redux-devtools-extension";
 import rootReducer from "./rootReducer";
 import { loadState, saveState } from "../models/localStorage";
 
-const store = createStore(rootReducer, loadState(), composeWithDevTools(applyMiddleware(logger, thunk)));
+const store = createStore(rootReducer, loadState(), applyMiddleware(logger, thunk));
 
 store.subscribe(() => {
   const state = store.getState();
